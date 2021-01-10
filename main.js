03da5c60efd20a8f0c604e7f4105c2ab0bb272e7
czetoj <czeto.jozsef.0229@gmail.com>
2021-01-10 18:59:33 +0100

    version 1.0

---
+++ main.js
@@ -0,0 +1,13 @@
+$('[data-toggle="tooltip"]').tooltip();
+const navbar = document.querySelector('.navbar');
+const navlinks = document.querySelectorAll('.cus--navlink');
+
+document.addEventListener('scroll', () => {
+    navbar.classList.remove('bg-transparent');
+    navbar.classList.add('cus--nav-white');
+    if (document.body.clientWidth >= 768) {
+        document.querySelector('.navbar-brand').style.color = 'grey';
+    }
+
+    navlinks.forEach(item => item.style.color = 'grey');
+})
